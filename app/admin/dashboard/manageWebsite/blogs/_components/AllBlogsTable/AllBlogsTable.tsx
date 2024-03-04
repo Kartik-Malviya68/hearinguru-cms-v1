da789a67c0119ed0a89110cb4568833d6887aed6
Xydon <snehalkumar.singh.cse20@itbhu.ac.in>
2024-03-04 18:10:05 +0530

    added interaction and fixed the checkbox

--- app/admin/dashboard/manageWebsite/blogs/_components/AllBlogsTable/AllBlogsTable.tsx
+++ app/admin/dashboard/manageWebsite/blogs/_components/AllBlogsTable/AllBlogsTable.tsx
@@ -2,113 +2,131 @@ import { Button, Pagination } from "flowbite-react";
 import React from "react";
 import Table from "../BlogTable/BlogTable";
 import {
-  flexRender,
-  getCoreRowModel,
-  getExpandedRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  useReactTable,
+	flexRender,
+	getCoreRowModel,
+	getExpandedRowModel,
+	getFilteredRowModel,
+	getPaginationRowModel,
+	useReactTable,
 } from "@tanstack/react-table";
 import { dummyData } from "../BlogTable/DummyData";
 import columns from "../BlogTable/TableColumn";
 import useHandleAsync from "@/modules/StateManagement/useHandleAsync/useHandleAsync";
 import fetchBlogData from "../../_fetch/services/getBlogData";
-function AllBlogsTable() {
-  const onPageChange = (page: number) => {
-    table.setState((old) => {
-      return {
-        ...old,
-        pagination: {
-          ...old.pagination,
-          pageIndex: page - 1,
-        },
-      };
-    });
-  };
-  const [rowSelection, setRowSelection] = React.useState({});
-  const [xstate, loading, fetcher] = useHandleAsync(fetchBlogData);
-  console.log(rowSelection)
-  React.useEffect(() => {
-    fetcher();
-  }, []);
-  const [columnFilters, setColumnFilters] = React.useState([
-    { id: "category", value: "" },
-  ]);
-  const table = useReactTable({
-    getFilteredRowModel: getFilteredRowModel(),
-    getCoreRowModel: getCoreRowModel(),
-    getExpandedRowModel: getExpandedRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    onRowSelectionChange: setRowSelection,
-    data: xstate || [], //dummyData,
-    columns: columns(),
-    state: {
-      columnFilters,
-      rowSelection,
-    },
-    initialState: {
-      pagination: {
-        pageIndex: 0,
-        pageSize: 6,
-      },
-    },
-  });
 
-  const buttonTabs = [
-    { id: 1, name: "All", value: "" },
-    { id: 2, name: "Hear loss", value: "Hear loss" },
-    { id: 3, name: "Types & Causes", value: "Types And Causes" },
-    { id: 4, name: "Symptoms", value: "Symptoms" },
-    { id: 5, name: "Prevention", value: "Prevention" },
-  ];
-  return (
-    <>
-      <div className="flex justify-between gap-5 w-full">
-        <div className="flex items-center">
-          {buttonTabs.map((tab) => (
-            <button
-              key={tab.id}
-              onClick={() =>
-                setColumnFilters([{ id: "category", value: tab.value }] as any)
-              }
-              className={`p-3 flex items-center text-blue-500 ${
-                columnFilters[0].value === tab.value
-                  ? "bg-[#e4efff]"
-                  : "bg-white"
-              } text-sm font-medium leading-150 rounded bg-[#e4efff] gap-[10px]`}
-            >
-              {tab.name}
-            </button>
-          ))}
-        </div>
-        <Button color="failure" className="w-[147px]" size={"sm"}>
-          Delete
-        </Button>
-      </div>
-      <div className="bg-white shadow w-full flex flex-col rounded-lg">
-        <Table table={table} flexRender={flexRender} />
-        <div className="w-full border-t p-4 justify-between flex items-center">
-          <h4 className="text-sm font-normal text-gray-500">
-            Showing{" "}
-            <span className="font-semibold text-gray-900">
-              {table.getState().pagination.pageIndex + 1}-{table.getRowCount()}
-            </span>{" "}
-            of{" "}
-            <span className="font-semibold text-gray-900">
-              {table.getRowCount()}
-            </span>
-          </h4>
-          <Pagination
-            color="blue"
-            currentPage={table.getState().pagination.pageIndex + 1}
-            totalPages={table.getPageCount()}
-            onPageChange={onPageChange}
-            showIcons
-          />
-        </div>
-      </div>
-    </>
-  );
+export interface AllBlogsTableInteraction {
+	filterRecordsFromQuery: (query: string) => void;
+}
+
+interface Props {
+	setInteraction?: (v: AllBlogsTableInteraction) => void;
+}
+
+function AllBlogsTable({ setInteraction }: Props) {
+	const onPageChange = (page: number) => {
+		table.setState((old) => {
+			return {
+				...old,
+				pagination: {
+					...old.pagination,
+					pageIndex: page - 1,
+				},
+			};
+		});
+	};
+	const [rowSelection, setRowSelection] = React.useState({});
+	const [xstate, loading, fetcher] = useHandleAsync(fetchBlogData);
+	console.log(rowSelection);
+	React.useEffect(() => {
+		fetcher();
+	}, []);
+	const [columnFilters, setColumnFilters] = React.useState([
+		{ id: "category", value: "" },
+	]);
+	const table = useReactTable({
+		getFilteredRowModel: getFilteredRowModel(),
+		getCoreRowModel: getCoreRowModel(),
+		getExpandedRowModel: getExpandedRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		onRowSelectionChange: setRowSelection,
+		data: xstate || [], //dummyData,
+		columns: columns(),
+		state: {
+			columnFilters,
+			rowSelection,
+		},
+		initialState: {
+			pagination: {
+				pageIndex: 0,
+				pageSize: 6,
+			},
+		},
+	});
+
+	const buttonTabs = [
+		{ id: 1, name: "All", value: "" },
+		{ id: 2, name: "Hear loss", value: "Hear loss" },
+		{ id: 3, name: "Types & Causes", value: "Types And Causes" },
+		{ id: 4, name: "Symptoms", value: "Symptoms" },
+		{ id: 5, name: "Prevention", value: "Prevention" },
+	];
+
+	setInteraction &&
+		setInteraction({
+			filterRecordsFromQuery: function (query: string): void {
+        //* write the filteration logic
+				console.log("filtering the table rows as per the query ", query);
+			},
+		});
+
+	return (
+		<>
+			<div className="flex justify-between gap-5 w-full">
+				<div className="flex items-center">
+					{buttonTabs.map((tab) => (
+						<button
+							key={tab.id}
+							onClick={() =>
+								setColumnFilters([{ id: "category", value: tab.value }] as any)
+							}
+							className={`p-3 flex items-center text-blue-500 ${
+								columnFilters[0].value === tab.value
+									? "bg-[#e4efff]"
+									: "bg-white"
+							} text-sm font-medium leading-150 rounded bg-[#e4efff] gap-[10px]`}
+						>
+							{tab.name}
+						</button>
+					))}
+				</div>
+				<Button color="failure" className="w-[147px]" size={"sm"}>
+					Delete
+				</Button>
+			</div>
+			<div className="bg-white shadow w-full flex flex-col rounded-lg">
+				<Table table={table} flexRender={flexRender} />
+				<div className="w-full border-t p-4 justify-between flex items-center">
+					<h4 className="text-sm font-normal text-gray-500">
+						Showing{" "}
+						<span className="font-semibold text-gray-900">
+							{table.getState().pagination.pageIndex + 1}-{table.getRowCount()}
+						</span>{" "}
+						of{" "}
+						<span className="font-semibold text-gray-900">
+							{table.getRowCount()}
+						</span>
+					</h4>
+					<Pagination
+						color="blue"
+						currentPage={table.getState().pagination.pageIndex + 1}
+						totalPages={table.getPageCount()}
+						onPageChange={onPageChange}
+						showIcons
+					/>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default AllBlogsTable;
