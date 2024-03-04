d3654846f11bde856748a5b40d175b00638dbd9e
Kartik Malviya <87184927+Kartik-Malviya68@users.noreply.github.com>
2024-03-04 20:31:18 +0530

    added image pop over

--- app/admin/dashboard/manageWebsite/blogs/_components/AllBlogsTable/AllBlogsTable.tsx
+++ app/admin/dashboard/manageWebsite/blogs/_components/AllBlogsTable/AllBlogsTable.tsx
@@ -1,13 +1,13 @@
 import { Button, Pagination } from "flowbite-react";
-import React from "react";
+import React, { useRef, useState } from "react";
 import Table from "../BlogTable/BlogTable";
 import {
-	flexRender,
-	getCoreRowModel,
-	getExpandedRowModel,
-	getFilteredRowModel,
-	getPaginationRowModel,
-	useReactTable,
+  flexRender,
+  getCoreRowModel,
+  getExpandedRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  useReactTable,
 } from "@tanstack/react-table";
 import { dummyData } from "../BlogTable/DummyData";
 import columns from "../BlogTable/TableColumn";
@@ -15,118 +15,125 @@ import useHandleAsync from "@/modules/StateManagement/useHandleAsync/useHandleAs
 import fetchBlogData from "../../_fetch/services/getBlogData";
 
 export interface AllBlogsTableInteraction {
-	filterRecordsFromQuery: (query: string) => void;
+  filterRecordsFromQuery: (query: string) => void;
 }
 
 interface Props {
-	setInteraction?: (v: AllBlogsTableInteraction) => void;
+  setInteraction?: (v: AllBlogsTableInteraction) => void;
 }
 
 function AllBlogsTable({ setInteraction }: Props) {
-	const onPageChange = (page: number) => {
-		table.setState((old) => {
-			return {
-				...old,
-				pagination: {
-					...old.pagination,
-					pageIndex: page - 1,
-				},
-			};
-		});
-	};
-	const [rowSelection, setRowSelection] = React.useState({});
-	const [xstate, loading, fetcher] = useHandleAsync(fetchBlogData);
-	console.log(rowSelection);
-	React.useEffect(() => {
-		fetcher();
-	}, []);
-	const [columnFilters, setColumnFilters] = React.useState([
-		{ id: "category", value: "" },
-	]);
-	const table = useReactTable({
-		getFilteredRowModel: getFilteredRowModel(),
-		getCoreRowModel: getCoreRowModel(),
-		getExpandedRowModel: getExpandedRowModel(),
-		getPaginationRowModel: getPaginationRowModel(),
-		onRowSelectionChange: setRowSelection,
-		data: xstate || [], //dummyData,
-		columns: columns(),
-		state: {
-			columnFilters,
-			rowSelection,
-		},
-		initialState: {
-			pagination: {
-				pageIndex: 0,
-				pageSize: 6,
-			},
-		},
-	});
+  const [checked, setChecked] = useState(true);
+  const onPageChange = (page: number) => {
+    table.setState((old) => {
+      return {
+        ...old,
+        pagination: {
+          ...old.pagination,
+          pageIndex: page - 1,
+        },
+      };
+    });
+  };
+  const [rowSelection, setRowSelection] = React.useState({});
+  const [xstate, loading, fetcher] = useHandleAsync(fetchBlogData);
+  console.log(rowSelection);
+  React.useEffect(() => {
+    fetcher();
+  }, []);
+  const [columnFilters, setColumnFilters] = React.useState([
+    { id: "category", value: "" },
+  ]);
+  const table = useReactTable({
+    getFilteredRowModel: getFilteredRowModel(),
+    getCoreRowModel: getCoreRowModel(),
+    getExpandedRowModel: getExpandedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onRowSelectionChange: setRowSelection,
+    data: xstate || [], //dummyData,
+    columns: columns({ setChecked }),
+    state: {
+      columnFilters,
+      rowSelection,
+    },
+    initialState: {
+      pagination: {
+        pageIndex: 0,
+        pageSize: 6,
+      },
+    },
+  });
 
-	const buttonTabs = [
-		{ id: 1, name: "All", value: "" },
-		{ id: 2, name: "Hear loss", value: "Hear loss" },
-		{ id: 3, name: "Types & Causes", value: "Types And Causes" },
-		{ id: 4, name: "Symptoms", value: "Symptoms" },
-		{ id: 5, name: "Prevention", value: "Prevention" },
-	];
+  const buttonTabs = [
+    { id: 1, name: "All", value: "" },
+    { id: 2, name: "Hear loss", value: "Hear loss" },
+    { id: 3, name: "Types & Causes", value: "Types And Causes" },
+    { id: 4, name: "Symptoms", value: "Symptoms" },
+    { id: 5, name: "Prevention", value: "Prevention" },
+  ];
 
-	setInteraction &&
-		setInteraction({
-			filterRecordsFromQuery: function (query: string): void {
-        //* write the filteration logic
-				console.log("filtering the table rows as per the query ", query);
-			},
-		});
+  const v = ""; // Declare the variable 'v' with an initial value
+  setInteraction &&
+    setInteraction({
+      filterRecordsFromQuery: function (query: string): void {
+        table.setGlobalFilter(query);
+      },
+    });
 
-	return (
-		<>
-			<div className="flex justify-between gap-5 w-full">
-				<div className="flex items-center">
-					{buttonTabs.map((tab) => (
-						<button
-							key={tab.id}
-							onClick={() =>
-								setColumnFilters([{ id: "category", value: tab.value }] as any)
-							}
-							className={`p-3 flex items-center text-blue-500 ${
-								columnFilters[0].value === tab.value
-									? "bg-[#e4efff]"
-									: "bg-white"
-							} text-sm font-medium leading-150 rounded bg-[#e4efff] gap-[10px]`}
-						>
-							{tab.name}
-						</button>
-					))}
-				</div>
-				<Button color="failure" className="w-[147px]" size={"sm"}>
-					Delete
-				</Button>
-			</div>
-			<div className="bg-white shadow w-full flex flex-col rounded-lg">
-				<Table table={table} flexRender={flexRender} />
-				<div className="w-full border-t p-4 justify-between flex items-center">
-					<h4 className="text-sm font-normal text-gray-500">
-						Showing{" "}
-						<span className="font-semibold text-gray-900">
-							{table.getState().pagination.pageIndex + 1}-{table.getRowCount()}
-						</span>{" "}
-						of{" "}
-						<span className="font-semibold text-gray-900">
-							{table.getRowCount()}
-						</span>
-					</h4>
-					<Pagination
-						color="blue"
-						currentPage={table.getState().pagination.pageIndex + 1}
-						totalPages={table.getPageCount()}
-						onPageChange={onPageChange}
-						showIcons
-					/>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="flex justify-between gap-5 w-full">
+        <div className="flex items-center">
+          {buttonTabs.map((tab) => (
+            <button
+              key={tab.id}
+              onClick={() =>
+                setColumnFilters([{ id: "category", value: tab.value }] as any)
+              }
+              className={`p-3 flex items-center text-blue-500 ${
+                columnFilters[0].value === tab.value
+                  ? "bg-[#e4efff] text-blue-500"
+                  : "bg-white text-gray-500"
+              } text-sm font-medium leading-150 rounded bg-[#e4efff] gap-[10px]`}
+            >
+              {tab.name}
+            </button>
+          ))}
+        </div>
+
+        <Button
+          color="failure"
+          disabled={checked}
+          className="w-[147px]"
+          size={"sm"}
+        >
+          Delete
+        </Button>
+      </div>
+      <div className="bg-white shadow w-full flex flex-col rounded-lg">
+        <Table table={table} flexRender={flexRender} />
+        <div className="w-full border-t p-4 justify-between flex items-center">
+          <h4 className="text-sm font-normal text-gray-500">
+            Showing{" "}
+            <span className="font-semibold text-gray-900">
+              {table.getState().pagination.pageIndex + 1}-{table.getRowCount()}
+            </span>{" "}
+            of{" "}
+            <span className="font-semibold text-gray-900">
+              {table.getRowCount()}
+            </span>
+          </h4>
+          <Pagination
+            color="blue"
+            currentPage={table.getState().pagination.pageIndex + 1}
+            totalPages={table.getPageCount()}
+            onPageChange={onPageChange}
+            showIcons
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AllBlogsTable;
--- app/admin/dashboard/manageWebsite/blogs/_components/BlogTable/TableColumn.tsx
+++ app/admin/dashboard/manageWebsite/blogs/_components/BlogTable/TableColumn.tsx
@@ -1,137 +1,207 @@
 import { createColumnHelper } from "@tanstack/react-table";
 import { Checkbox } from "flowbite-react";
-import { Dialog, Transition } from "@headlessui/react";
+import { Dialog, Popover, Transition } from "@headlessui/react";
 import Image from "next/image";
 import { Fragment, useState } from "react";
 
+const imagePopoverLinks = [
+  {
+    title: "View Blog",
+    link: "",
+    onClick: () => {},
+  },
+  {
+    title: "Edit Details",
+    link: "",
+    onClick: () => {},
+  },
+  {
+    title: "View Image",
+    link: "",
+    onClick: () => {},
+  },
+];
+
 const columnHelper = createColumnHelper<BlogTypes.BlogTable>();
-
 import React from "react";
+import Link from "next/link";
 
-const columns = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	const [selected, setSelected] = useState("");
-	function closeModal() {
-		setIsOpen(false);
-	}
+const columns = ({ setChecked }: { setChecked: (e: any) => void }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState("");
 
-	function openModal() {
-		setIsOpen(true);
-	}
-	const columnsLayout = [
-		columnHelper.display({
-			id: "select",
-			header: ({ table }) => {
-				return (
-					<Checkbox
-						checked={table.getIsAllPageRowsSelected()}
-						onChange={(value) => {
-							table.toggleAllPageRowsSelected(value.target.checked);
-						}}
-						color={"blue"}
-					/>
-				);
-			},
-			cell: ({ row }) => {
-				return (
-					<Checkbox
-						checked={row.getIsSelected()}
-						onChange={(value) => {
-							row.toggleSelected(value.target.checked);
-						}}
-						color={"blue"}
-					/>
-				);
-			},
+  function closeModal() {
+    setIsOpen(false);
+  }
 
-			enableSorting: false,
-			enableHiding: false,
-		}),
-		columnHelper.accessor("title", {
-			header: () => "Title",
-			cell: (info) => info.getValue(),
-		}),
-		columnHelper.accessor("slug", {
-			header: () => "Slug",
-			cell: (info) => info.getValue(),
-		}),
-		columnHelper.accessor("date", {
-			header: () => "Date",
-			cell: (info) => info.getValue().toLocaleString("en-IN"),
-		}),
-		columnHelper.accessor("category", {
-			header: () => "Category",
-			cell: (info) => info.getValue(),
-		}),
-		columnHelper.accessor("mainImage", {
-			header: () => "Main Image",
-			cell: (info) => {
-				return (
-					<>
-						<div className="relative w-[110px] h-[42px]">
-							<Image
-								onClick={() => {
-									setSelected(info.getValue().toString()), openModal();
-									console.log(selected);
-								}}
-								alt="blogImg"
-								style={{
-									borderRadius: "5px",
-									objectFit: "cover",
-									cursor: "pointer",
-								}}
-								layout="fill"
-								src={info.getValue() as string}
-							></Image>
-						</div>
-						<Transition appear show={isOpen} as={Fragment}>
-							<Dialog as="div" className="relative z-50" onClose={closeModal}>
-								<Transition.Child
-									as={Fragment}
-									enter="ease-out duration-300"
-									enterFrom="opacity-0"
-									enterTo="opacity-100"
-									leave="ease-in duration-200"
-									leaveFrom="opacity-100"
-									leaveTo="opacity-0"
-								>
-									<div className="fixed inset-0 bg-black/25" />
-								</Transition.Child>
+  function openModal() {
+    setIsOpen(true);
+  }
+  const columnsLayout = [
+    columnHelper.display({
+      id: "select",
+      header: ({ table }) => {
+        return (
+          <Checkbox
+            checked={table.getIsAllPageRowsSelected()}
+            onChange={(value) => {
+              table.toggleAllPageRowsSelected(value.target.checked);
+              setChecked(value.target.checked);
+            }}
+            color={"blue"}
+          />
+        );
+      },
+      cell: ({ row }) => {
+        return (
+          <Checkbox
+            checked={row.getIsSelected()}
+            onChange={(value) => {
+              row.toggleSelected(value.target.checked);
+              setChecked(value.target.checked);
+            }}
+            color={"blue"}
+          />
+        );
+      },
 
-								<div className="fixed inset-0 overflow-y-auto">
-									<div className="flex min-h-full items-center justify-center p-4 text-center">
-										<Transition.Child
-											as={Fragment}
-											enter="ease-out duration-300"
-											enterFrom="opacity-0 scale-95"
-											enterTo="opacity-100 scale-100"
-											leave="ease-in duration-200"
-											leaveFrom="opacity-100 scale-100"
-											leaveTo="opacity-0 scale-95"
-										>
-											<Dialog.Panel className=" max-w-[900px] transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-												<div className="w-[800px] h-[500px] relative">
-													<Image
-														onClick={openModal}
-														alt="blogImg"
-														style={{ borderRadius: "5px", objectFit: "cover" }}
-														layout="fill"
-														src={selected}
-														quality={100}
-													></Image>
-												</div>
-											</Dialog.Panel>
-										</Transition.Child>
-									</div>
-								</div>
-							</Dialog>
-						</Transition>
-					</>
-				);
-			},
-		}),
-	];
-	return columnsLayout;
+      enableSorting: false,
+      enableHiding: false,
+    }),
+    columnHelper.accessor("title", {
+      header: () => "Title",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("slug", {
+      header: () => "Slug",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("date", {
+      header: () => "Date",
+      cell: (info) => info.getValue().toLocaleString("en-IN"),
+    }),
+    columnHelper.accessor("category", {
+      header: () => "Category",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("mainImage", {
+      header: () => "Main Image",
+      cell: (info) => {
+        return (
+          <>
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  <Popover.Button>
+                    <div className="relative w-[110px] h-[42px]">
+                      <Image
+                        // onClick={() => {
+                        //   setSelected(info.getValue().toString()), openModal();
+                        //   console.log(selected);
+                        // }}
+                        alt="blogImg"
+                        style={{
+                          borderRadius: "5px",
+                          objectFit: "cover",
+                          cursor: "pointer",
+                        }}
+                        layout="fill"
+                        src={info.getValue() as string}
+                      />
+                    </div>
+                  </Popover.Button>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute  z-10 mt-3  w-[150px] -translate-x-1/2 transform px-4 sm:px-0">
+                      <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5">
+                        <div className="relative flex flex-col gap-7 p-4 bg-white">
+                          
+                          <Link
+                            href=""
+                            className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500/50"
+                          >
+                            View Blog
+                          </Link>
+                          <Link
+                            href=""
+                            className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500/50"
+                          >
+                            Edit Details
+                          </Link>
+                          <Link
+                            className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500/50"
+                            href=""
+                            onClick={() => {
+                              setSelected(info.getValue().toString()),
+                                openModal();
+                              console.log(selected);
+                            }}
+                          >
+                            View Image
+                          </Link>
+                        </div>
+                      </div>
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+
+            <Transition appear show={isOpen} as={Fragment}>
+              <Dialog as="div" className="relative z-50" onClose={closeModal}>
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="fixed inset-0 bg-black/25" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 overflow-y-auto">
+                  <div className="flex min-h-full items-center justify-center p-4 text-center">
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-out duration-300"
+                      enterFrom="opacity-0 scale-95"
+                      enterTo="opacity-100 scale-100"
+                      leave="ease-in duration-200"
+                      leaveFrom="opacity-100 scale-100"
+                      leaveTo="opacity-0 scale-95"
+                    >
+                      <Dialog.Panel className=" max-w-[900px] transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                        <div className="w-[800px] h-[500px] relative">
+                          <Image
+                            onClick={openModal}
+                            alt="blogImg"
+                            style={{ borderRadius: "5px", objectFit: "cover" }}
+                            layout="fill"
+                            src={selected}
+                            quality={100}
+                          ></Image>
+                        </div>
+                      </Dialog.Panel>
+                    </Transition.Child>
+                  </div>
+                </div>
+              </Dialog>
+            </Transition>
+          </>
+        );
+      },
+    }),
+  ];
+  return columnsLayout;
 };
 
 export default columns;
